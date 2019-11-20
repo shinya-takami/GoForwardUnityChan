e263819594bde5c5ec698d15b4594e411bb50c75
高見慎哉 <>
2019-11-21 01:18:55 +0900

    Lesson7課題
    キューブが地面に接触する時とキューブ同士が積み重なるときに効果音をつける
    ユニティちゃんと衝突した時には効果音が鳴らないようにする

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour
+{
+    public AudioClip sound;
+
+    // キューブのPrefab
+    public GameObject cubePrefab;
+
+    void Start() { }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+}
